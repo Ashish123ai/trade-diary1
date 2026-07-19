0fdc807077be5c692dad434f3cac3a554e576038
Your Name <chaurasiyaveer9@gmail.com>
2026-07-19 16:07:40 +0530

    Fix Gmail SMTP connection - force IPv4

--- backend/mailer.js
+++ backend/mailer.js
@@ -2,11 +2,14 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD
-  }
+  },
+  family: 4 // force IPv4 -- Render's network can't reach Gmail over IPv6
 });
 
 async function sendOtpEmail(to, otp) {
